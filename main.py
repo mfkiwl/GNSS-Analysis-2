b762be62982e4bca31e1b7b6e85543eeaf8ad82d
LuizFillip <luizfillip@outlook.com>
2022-08-21 18:22:40 -0300

    Adding pipeline of ionospheric piercing points coordinates

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime
-
+from load import *
 
 class observables(object):
     
@@ -26,3 +26,49 @@ class observables(object):
         self.l2lli = obs.L2lli.values.astype(int)
         
         self.time = pd.to_datetime(obs.index.get_level_values('time'))
+        
+def piercing_points_data(infile, 
+                         filename, 
+                         prn, 
+                         obs_x, obs_y, obs_z):
+
+    ob = load_orbits(infile, 
+                     filename, 
+                     prn = prn)
+
+    sat_x_vals = ob.position("x").values.ravel()
+    sat_y_vals = ob.position("y").values.ravel()
+    sat_z_vals = ob.position("z").values.ravel()
+
+    times = ob.position("x").index
+
+    lon, lat, alt = convert_coords(obs_x, obs_y, obs_z)
+
+    result = { "lon": [], "lat": [], "time": []}
+
+
+    for num in range(len(times)):
+
+        sat_x, sat_y, sat_z, time = sat_x_vals[num], sat_y_vals[num], sat_z_vals[num], times[num]
+
+        ip = IonosphericPiercingPoint(sat_x, sat_y, sat_z, 
+                                      obs_x, obs_y, obs_z)
+
+        elevation = ip.elevation(lat, lon)
+
+        lat_ip, lon_ip = ip.ionospheric_sub_point(lat, lon)
+
+        if elevation > 0:
+
+            result['lon'].append(lon_ip)
+            result['lat'].append(lat_ip)
+            result['time'].append(time)
+
+    ip = pd.DataFrame(result)
+
+    ip.index = ip.time
+
+    del ip["time"]
+
+    return ip
+
