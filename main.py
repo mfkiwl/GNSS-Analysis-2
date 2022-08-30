d02d758199856338a71e7d49de09286b4c404155
LuizFillip <luizfillip@outlook.com>
2022-08-30 12:41:29 -0300

    Fixing dcb section and adding prn list

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 from sub_ionospheric_point import piercing_points_data
 from relative_tec_calculator import relative_tec_data
-from dcb_calculator import load_dcb
+from dcb_calculator import get_cdb_value, create_prns
 import pandas as pd
+from utils import get_paths
+import time
 
 
 
-
-
-def process_data(path_tec: str, 
-                path_orbit: str, 
-                path_dcb: str,
-                obs: list,
-                prn: str):
+def process_data(year:int, 
+                 station:str,
+                 obs: list,
+                 prn: str):
     
     """
     Concat the relative TEC for each piercing point 
     and compute other variables
     """
-    
-    
-    sat_bias = load_dcb(path_dcb).value_tec
+
+    path_tec, path_orbit, path_dcb = get_paths(year, station)
+ 
+    sat_bias = get_cdb_value(path_dcb, prn)
 
     tecData = relative_tec_data(path_tec, prn = prn)
     
@@ -32,54 +32,50 @@ def process_data(path_tec: str,
     
     df["vTEC"] = df["proj"] * df["cTEC"]
     
-    #df = df.dropna(subset = ["lat", "lon", "el"])
-    
-    df.columns.names = [prn]
+    df['prn'] = prn
+    #df.set_index('prn', append = True, inplace = True)
     
     
     return df
 
 
 
-def run_for_all_prns():
+def run_for_all_prns(year, station, obs, doy, save = True):
+
+    result = []
+    
+    for prn in create_prns():
+        try: 
+            result.append(process_data(year, station, obs, prn))
+        except:
+            continue
+            print(f"The {prn} doesnt work!")
+    
+    df = pd.concat(result)
+    
+    if save:
+        filename = f"{station}{doy}"
+        df.to_csv(f"Database/all_process/{year}/{filename}.txt", 
+                                  sep = " ", index = True)
+    return df
+
+def main():
+    start_time = time.time()
+    
+    
     # Arapiraca
     #obs_x, obs_y, obs_z = 5043729.726, -3753105.556, -1072967.067
     
     # Belo Horizonte
     obs_x, obs_y, obs_z = 4320741.822, -4161560.476, -2161984.249
-
+    
     obs = list((obs_x, obs_y, obs_z))
     
+    year = 2015
+    station = "mgbh"
+    doy = 131
     
-    path_tec = "Database/process/2015/mgbh1311.15o.txt"
-    path_orbit = "Database/orbit/2015/igr18441.sp3/igr18441.sp3"
-    path_dcb = "Database/dcb/2015/CAS0MGXRAP_20151310000_01D_01D_DCB.BSX"
+    run_for_all_prns(year, station, obs, doy, save = True)
     
-    result = []
-    for num in range(1, 33):
-        if num < 10:
-            prn = f"G0{num}"
-        else:
-            prn = f"G{num}"
-       
-  
-        try: 
-            df = process_data(path_tec, 
-                           path_orbit, 
-                           path_dcb,
-                           obs, 
-                           prn)
-            
-            df['prn'] = prn
-            df.set_index('prn', append=True, inplace=True)
-            result.append(df)
-        except:
-            continue
-            print(f"In {prn} doesnt work")
     
-    return pd.concat(result)
-    
-
-filename = "mgbh1311.15o"
-run_for_all_prns().to_csv(f"Database/all_process/2015/{filename}.txt", 
-                          sep = " ", index = True)
+    print("--- %s seconds ---" % (time.time() - start_time))
