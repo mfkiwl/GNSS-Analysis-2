30469274127cf7ceebee2ddb8b70671b96f347b3
LuizFillip <luizfillip@outlook.com>
2022-08-30 17:13:24 -0300

    Fixing process data function (variables names, columns and paths)

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from sub_ionospheric_point import piercing_points_data
 from relative_tec_calculator import relative_tec_data
 from dcb_calculator import get_cdb_value, create_prns
 import pandas as pd
-from utils import get_paths
+from utils import get_paths, doy_str_format
 import time
 
 
 
 def process_data(year:int, 
+                 doy: int,
                  station:str,
                  obs: list,
                  prn: str):
@@ -17,23 +18,22 @@ def process_data(year:int,
     and compute other variables
     """
 
-    path_tec, path_orbit, path_dcb = get_paths(year, station)
- 
+    path_tec, path_orbit, path_dcb = get_paths(year, doy, station)
+     
     sat_bias = get_cdb_value(path_dcb, prn)
 
     tecData = relative_tec_data(path_tec, prn = prn)
-    
-    tecData["cTEC"] = tecData["rTEC"] - sat_bias
       
     ippData = piercing_points_data(path_orbit, obs, prn = prn)
     
+    df = tecData.join(ippData).interpolate(method = 
+                                           'nearest').ffill().bfill()
     
-    df = tecData.join(ippData).interpolate()
-    
-    df["vTEC"] = df["proj"] * df["cTEC"]
+    df["vtec"] = df["proj"] * df["stec"]
     
     df['prn'] = prn
-    #df.set_index('prn', append = True, inplace = True)
+    
+    df["bias"] = sat_bias
     
     
     return df
@@ -54,7 +54,7 @@ def run_for_all_prns(year, station, obs, doy, save = True):
     df = pd.concat(result)
     
     if save:
-        filename = f"{station}{doy}"
+        filename = f"{station}{doy_str_format(doy)}"
         df.to_csv(f"Database/all_process/{year}/{filename}.txt", 
                                   sep = " ", index = True)
     return df
@@ -67,15 +67,32 @@ def main():
     #obs_x, obs_y, obs_z = 5043729.726, -3753105.556, -1072967.067
     
     # Belo Horizonte
-    obs_x, obs_y, obs_z = 4320741.822, -4161560.476, -2161984.249
+    #obs_x, obs_y, obs_z = 4320741.822, -4161560.476, -2161984.249
+    
+    # Fortaleza
+    
+    obs_x, obs_y, obs_z = 4983062.7560, -3959862.9020,  -410039.5900
     
     obs = list((obs_x, obs_y, obs_z))
+
     
-    year = 2015
-    station = "mgbh"
-    doy = 131
+    year = 2014
+    station = "ceft"
+    doy = 1
+    prn = "G01"
     
-    run_for_all_prns(year, station, obs, doy, save = True)
+   
+    df = process_data(year, 
+                     doy,
+                     station,
+                     obs,
+                     prn)
+    
+    print(df)
+    
+    #run_for_all_prns(year, station, obs, doy, save = True)
     
     
     print("--- %s seconds ---" % (time.time() - start_time))
+    
+main()
