e0d37836e1c784cc3d4eee64006fead880719ab5
LuizFillip <luizfillip@outlook.com>
2022-09-04 20:25:08 -0300

    Plotting directory

---
+++ plot/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 31 13:25:52 2022
+
+@author: Luiz
+"""
+
---
+++ plot/plotTECmaps.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  4 20:23:57 2022
+
+@author: Luiz
+"""
+
+from plot.mapping import *
+
+start_lat = -40
+end_lat = 10
+end_lon = -30
+start_lon = -80
+fig, ax = plotMapping(start_lat = start_lat, 
+                end_lat = end_lat, 
+                start_lon = start_lon, 
+                end_lon = end_lon,
+                step_lat = 5, 
+                step_lon = 5)
+
+num = 30
+elevation = 0
+
+sel_time = times[num]
+
+df = adf.loc[(adf.index > sel_time) & 
+             (adf.index < sel_time + delta) & 
+             (adf.el > elevation), :]
+
+ts = df.copy()
+
+lons, lats, arr = getting_values(ts, 
+               start_lat, end_lat, 
+               start_lon, end_lon, 
+               step = 1)
+
+ax.contourf(lons, lats, arr, 50, cmap = "jet")
+
+ax.set(title = str(sel_time))
+plt.show()
