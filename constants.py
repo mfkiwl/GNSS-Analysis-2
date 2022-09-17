4b23e5a60bab9bb335329cf49c2fc24ba8890fa0
LuizFillip <luizfillip@outlook.com>
2022-09-17 15:17:25 -0300

    Fixing factor_mw

--- constants.py
+++ constants.py
@@ -1,34 +1,75 @@
+import numpy as np
+
 class constants(object):
     
     """Contants used in the GNSS routines"""
     
-    # Fundamental constants
-    F1 = 1575.42e6 
-    F2 = 1227.60e6
+    
+    # Basic constants
     c = 299792458.0
-     
     TECU = 1.0e16 
     A = 40.3  
+        
     
-    DIFF_TEC_MAX = 0.05
-    pmean = 0.0
-    pdev = DIFF_TEC_MAX * 4.0
-    
-    
+        
+        
     # Geodesic constants
     alt_bottom = 6620.0
     alt_top = 6870.0
-
+    
     radius_earth = 6371.0  
     avg_heigth = 250.0 
-    
-
+        
     dtor = 0.0174533
+        
+    @staticmethod
+    def frequency(prn):
+        """Compute fundamental frequencies"""
+        if prn[0] == "G":
+            
+            F1 = 1575.42e6 
+            F2 = 1227.60e6
+            
+        elif prn[0] == "R":
+            
+            numbers = [1, -4, 5, 6, 1, -4, 5, 
+                       6, -2, -7, 0,-1,-2,-7,
+                       0,-1,4,-3,3,2,4,-3,3,2, 
+                       np.nan,-6]
+            
+            num = int(prn[1:]) - 1
+            
+            F1 = 1602e6 + numbers[num] * 562.5e3
+            F2 = 1246e6 + numbers[num] * 437.5e3
+            
+        return F1, F2
     
-    factor = (F1 - F2) / (F1 + F2) / c
+    @staticmethod
+    def factor(prn):
+        const = constants()
+        F1, F2 = const.frequency(prn)
+        return (F1 - F2) / (F1 + F2) / const.c
     
-    factor_mw = (F1 * F2) / (F2 - F1) / c
+    @staticmethod
+    def factor_mw(prn):
+        const = constants()
+        F1, F2 = const.frequency(prn)
+        return (F1 * F2) / (F2 - F1) / const.c
     
-    factor_TEC = ((F1 * F2) ** 2) / (F1 + F2) / (F1 - F2) / A / TECU
+    @staticmethod
+    def factor_TEC(prn):
+        const = constants()
+        F1, F2 = const.frequency(prn)
+        return ((F1 * F2) ** 2) / (F1 + F2) / (F1 - F2) / const.A / const.TECU
+    
+    
+    
+        
+        
         
    
+
+
+
+print(constants().factor("G01"))
+
