1503d1338910bf9f0a152a8b09bba5e846207262
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2022-09-19 11:56:56 -0300

    First processing step

--- main.py
+++ main.py
@@ -1,111 +1,95 @@
-from sub_ionospheric_point import piercing_points_data
+from load import load_receiver, save_attrs
 from relative_tec_calculator import relative_tec_data
-from dcb_calculator import get_cdb_value
-import pandas as pd
-from utils import create_prns, create_directory
+from build import paths, prns, folder
 import time
-import json
 import os
-from build import build_paths
-
-def process_data(year:int, 
-                 doy: int,
-                 station: str,
-                 obs: list,
-                 prn: str, 
-                 use_dcb: bool = False) -> pd.DataFrame:
-    
-    """
-    Concat the relative TEC for each piercing point 
-    for one single PRN
-    """
-    path = build_paths(year, doy)
-    
-    path_tec = path.fn_process(station)
-    path_orbit = path.fn_orbit()
-    
-    tec = relative_tec_data(path_tec, prn = prn)
-      
-    ipp = piercing_points_data(path_orbit, obs, prn = prn)
-    
-    if use_dcb:
-        path_dcb = path.fn_dcb()
-             
-        tec["bias"] = get_cdb_value(path_dcb, prn)
-
-    df = tec.join(ipp).interpolate(method ='nearest').ffill().bfill()
-    
-    df["vtec"] = df["proj"] * df["stec"]
-    
-    df['prn'] = prn
-        
-    df.columns.names = [station]
-    
-    return df
+from tqdm import tqdm
+import pandas as pd
 
 
+def run_for_all_prns(path, filename, save = True):
+    
+    infile = os.path.join(path.rinex, filename)
+    
+    data = load_receiver(infile)
+    
+    out_prns = []
+    
+    try:
+        prns_list = data.prns
+    except:
+        prns_list = prns().gps_and_glonass
 
-def run_for_all_prns(year: int, 
-                     doy: int, 
-                     station: str, 
-                     positions: list, 
-                     save: bool = True) -> pd.DataFrame:
-    
-    """Run for all prns and concatenate them"""
-
-    result = []
-    
-    path_to_save = build_paths(year, doy).fn_all_process(station)
-    
-    for prn in create_prns():
-        try: 
-            result.append(process_data(year, doy, station, positions, prn))
+    for prn in tqdm(prns_list, desc = filename):
+        try:
+            out_prns.append(relative_tec_data(data.df, prn = prn))
         except:
-            print(f"The {prn} doesnt work!")
             continue
-            
-    
-    df = pd.concat(result)
-    
+
+    all_prns = pd.concat(out_prns, axis = 1)
+
     if save:
-        df.to_csv(path_to_save, sep = " ", index = True)
-        
-    return df
-
-def run_for_all_stations(year: int, doy: int):
-    
-    """Processed the data for all stations"""   
-    path = build_paths(year, doy)
-    
-    path_created = create_directory(path.all_process)
-    
-    json_path = open(path.fn_json)
-
-    dat = json.load(json_path)
-    
-    _, _, files = next(os.walk(path.process))
-        
-    for filename in files:
-    
         station = filename[:4]
-            
-        positions = dat[station]["position"]
-   
-        df = run_for_all_prns(year, doy, station, positions, save = True)
+        path_to_save = path.fn_process(station)
+        all_prns.to_csv(path_to_save, sep = ";", index = True)
         
-        print(f"The station: {station} finishied")
+    return prns_list, data.attrs
+
+
+def run_for_all_files(path):
     
+    _, _, files = next(os.walk(path.rinex))
     
+    out_prns = []
+    out_dict = {}
+    create_folder = folder(path.process)
+        
+    for filename in files[:1]:
+        
+        if filename.endswith(f".{path.ext_rinex}"):
+            try:
+                prns, attr_dat = run_for_all_prns(path, filename)
+                out_dict.update(attr_dat)
+                out_prns.append(pd.DataFrame({filename[:4]: prns}))
+            except:
+                print(f"Station {filename} not work")
+                continue
+
+    return out_dict, pd.concat(out_prns, axis = 1)
+
+
+def run_for_all_days(year, root, start = 1, 
+                     end = 365, save_prn = True):
     
-def main():
+    prn_in_year = []
     
-   start_time = time.time()
+    for doy in range(start, end + 1):
     
-   year = 2022
-   
-   for doy in range(2, 5):
-       run_for_all_stations(year, doy)
-   
-   print("--- %s seconds ---" % (time.time() - start_time))
-main()
+        path = paths(year, doy)
+        
+        json_dat, prn_dat = run_for_all_files(path)
+        
+        prn_dat["doy"] =  doy
+    
+        prn_in_year.append(prn_dat)
+        
+        save_attrs(path.fn_json, json_dat)
+         
+    if save_prn:
+        prns_dat = pd.concat(prn_in_year)
+        prn_dat.to_csv(f"prns_in_{year}", sep = " ")
+        
+
+start_time = time.time()
+ 
+year = 2014
+root = "C:\\Users\\Public\\"
+
+run_for_all_days(year, root, start = 1, end = 365, save_prn = True)
+
+        
+
+
+print("--- %s hours ---" % ((time.time() - start_time) / 3600))
+
 
