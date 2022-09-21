3f04c439e58f18ecbebde904e7a8287de1645aa4
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2022-09-21 17:42:14 -0300

    Coupling into function

--- plot/plotStations.py
+++ plot/plotStations.py
@@ -1,38 +1,26 @@
 
 
 from sub_ionospheric_point import convert_coords
-import matplotlib.pyplot as plt
-from mapping import plotMapping
 import json
 
-path_json = 'Database/json/stations.json'
 
 
-f = open(path_json)
-dat = json.load(f)
-
-stations = list(dat.keys())
-
-start_lat = -35
-end_lat = 5
-start_lon = -75
-end_lon = -30
-
-fig, ax = plotMapping(start_lat = start_lat, 
-                     start_lon = start_lon, 
-                     end_lat = end_lat,
-                     end_lon = end_lon)
-
-for station in stations:
-    positions = dat[station]["position"] 
-    obs_x, obs_y, obs_z = tuple(positions)
-
-    coords = convert_coords(obs_x, obs_y, obs_z, to_radians = False)
-    lon, lat, alt = coords
-    
-    ax.plot(lon, lat, "s", markersize = 7, color = "red")
+def plotStations(ax, year, color = "green", markersize = 5, marker = "o"):
     
     
-ax.legend(["RBMC"])
+    path_json = 'Database/json/2014/001.json'
 
-plt.show()
+
+    dat = json.load(open(path_json))
+
+    stations = list(dat.keys())
+
+    for station in stations:
+        positions = dat[station]["position"] 
+        obs_x, obs_y, obs_z = tuple(positions)
+    
+        coords = convert_coords(obs_x, obs_y, obs_z, to_radians = False)
+        lon, lat, alt = coords
+        
+        ax.plot(lon, lat, marker = marker, markersize = markersize, color = color)
+    
