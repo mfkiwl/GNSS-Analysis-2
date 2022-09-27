603043ead9ca2ff7105483437d220f6bdb79ce4d
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2022-09-27 18:36:27 -0300

    Join into a function

--- plot/plotTRIObservation.py
+++ plot/plotTRIObservation.py
@@ -10,7 +10,9 @@ import matplotlib.dates as dates
 from plot.plotTECmaps import plotTECmap
 import cv2
 
-def load_and_plot_roti(ax, rotiInfile, start_date, end_date, delta = 2):
+def load_and_plot_roti(ax, rotiInfile, 
+                       start_date, end_date, 
+                       delta = 2):
 
 
     df = pd.read_csv(rotiInfile, 
@@ -25,7 +27,7 @@ def load_and_plot_roti(ax, rotiInfile, start_date, end_date, delta = 2):
                  (df.index <= end_date), :]
 
     col = "total"
-    ax.plot(df2[col], color = "k")
+    ax.plot(df2[col], color = "k", lw = 2)
     
     year, month, day = start_date.year, start_date.month, start_date.day 
     
@@ -33,9 +35,10 @@ def load_and_plot_roti(ax, rotiInfile, start_date, end_date, delta = 2):
     ax.xaxis.set_major_formatter(dates.DateFormatter('%H:%M'))
     ax.xaxis.set_major_locator(dates.HourLocator(interval = 1))
     
-    #start_del = datetime.datetime(year, month, day, 22, n)
-    #end_del = datetime.datetime(year, month, day, 22, n + delta)
-    #ax.axvspan(start_del, end_del, alpha=0.2, color = "gray")
+    return df2
+ 
+def shade(ax, start, end):
+    ax.axvspan(start, end, alpha=0.2, color = "gray")
     
 def plot_image(infile, filename, ax):
 
@@ -43,64 +46,127 @@ def plot_image(infile, filename, ax):
     
     ax.imshow(img, aspect = "auto")
     ax.set(xticks = [], yticks = [])
-
-plt.figure(figsize=(18, 9))
-G = gridspec.GridSpec(3, 3)
-
-ax1 = plt.subplot(G[0, :])
-
-
-rotiInfile = "results/database/max_rotis/"
-
-_,_, roti_files = next(os.walk(rotiInfile))
     
-rotiFilename = roti_files[0]
+    
+def get_files(infile): 
+    _, _, files = next(os.walk(infile))
+    return files
 
-rotiInfile = rotiInfile + rotiFilename
+def imager_fname(filename):
+    """Convert filename attributes into datetime format"""
+    if ".tif" in filename:
+        infos = filename.replace(".tif", "").split("_")
+    else:
+        infos = filename.replace(".png", "").split("_")
+
+    date = infos[2]
+    time = infos[-1]
+    return datetime.datetime.strptime(date + " " + time, '%Y%m%d %H%M%S')
+
+def ionosonde_fname(filename):
+    """Convert digisonde filename (EMBRACE format) to datetime"""
+    args = filename.split("_")
+    code = args[0]
+    date = args[1][:8]
+    time = args[1][13:-4]
+    return datetime.datetime.strptime(date + time, "%Y%m%d%H%M%S")
+
+
+def tec_fname(filename):
+    """Convert TEC filename (EMBRACE format) to datetime"""
+    args = filename.split('_')
+    date = args[1][:4] + '-' + args[1][4:6]+ '-' +args[1][-2:] 
+    time = args[-1].replace('.txt', '')
+    time = time[:2] + ':' + time[2:]
+    
+    return datetime.datetime.strptime(date + ' ' + time, "%Y-%m-%d %H:%M")
+
+
+
+
+
+
+
+
+
 
 year, month = 2014, 1
 
 day = 1
 
-start_date = datetime.datetime(year, month, day, 21, 0)
-end_date = datetime.datetime(year, month, day + 1, 7, 0)
-
-load_and_plot_roti(ax1, rotiInfile, start_date, end_date, delta = 2)
 
 
-
-ax2 = plt.subplot(G[1:, -1],  projection = ccrs.PlateCarree())
-
-
+rotiInfile = "G:\\My Drive\\Python\\data-analysis\\results\\database\\max_rotis\\"
 tecInfile = "C:\\TEC_2014\\TEC_2014_01\\"
-
-
-_, _, tec_files = next(os.walk(tecInfile))
-
-tecFilename = tec_files[0]
-
-plotTECmap(ax2, tecInfile, tecFilename)
-
-ax3 = plt.subplot(G[1:, 0])
-
 ionoInfile = "G:\\My Drive\\Python\\data-analysis\\digisonde\\img\\2014_test\\"
-
-_, _, iono_files = next(os.walk(ionoInfile))
-
-ionoFilename = iono_files[0]
-
-plot_image(ionoInfile, ionoFilename,  ax3)
-
-ax4 = plt.subplot(G[1:, -2])
-
-
 imagerInfile = "G:/My Drive/Python/data-analysis/imager/database/2014/001_test/"
 
-_, _, imager_files = next(os.walk(imagerInfile))
+tec_files = get_files(tecInfile)
+tecFilename = tec_files[0]
 
+iono_files = get_files(ionoInfile)
+ionoFilename = iono_files[0]
+
+imager_files =  get_files(imagerInfile)
 imagerFilename = imager_files[0]
 
-plot_image(imagerInfile, imagerFilename, ax4)
 
-plt.tight_layout()
-plt.show()
+roti_files = get_files(rotiInfile)
+rotiFilename = roti_files[0]
+
+
+def plot_quadri_observation(imagerInfile, 
+                            ionoInfile, 
+                            tecInfile, 
+                            imagerFilename, 
+                            ionoFilename, 
+                            tecFilename, save = False):
+    
+    if save:
+        plt.ioff()
+        
+        
+    fig = plt.figure(figsize = (18, 9))
+    
+    plt.subplots_adjust(wspace = 0.2, hspace = 0.5)
+    G = gridspec.GridSpec(3, 3)
+    
+    ax1 = plt.subplot(G[0, :])
+    
+    start_date = datetime.datetime(year, month, day, 21, 0)
+    end_date = datetime.datetime(year, month, day + 1, 7, 0)
+    
+    df = load_and_plot_roti(ax1, rotiInfile + rotiFilename, start_date, 
+                            end_date, delta = 2)
+    
+    
+    ax2 = plt.subplot(G[1:, -1],  projection = ccrs.PlateCarree())
+    
+    
+    plotTECmap(ax2, tecInfile, tecFilename)
+    
+    
+    shade(ax1,
+          datetime.datetime(2014, 1, 1, 22, 0), 
+          datetime.datetime(2014, 1, 1, 22, 10))
+    
+    ax3 = plt.subplot(G[1:, 0])
+    
+    
+    
+    plot_image(ionoInfile, ionoFilename, ax3)
+    
+    ax4 = plt.subplot(G[1:, -2])
+    
+    
+    
+    plot_image(imagerInfile, imagerFilename, ax4)
+    
+    if save:
+        filename = imager_fname(imagerFilename)
+        print("Saving...", filename)
+        name_to_save = filename.strftime("%Y%m%d%H%M%S")
+        fig.savefig(f"C:\\tri_observation\\{name_to_save}.png", 
+                   dpi = 80, bbox_inches="tight")
+    else:
+        plt.show()
