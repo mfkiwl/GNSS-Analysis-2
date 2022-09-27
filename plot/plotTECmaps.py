91b11c14db8237e18a698ef4791c1f22d9b54a77
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2022-09-27 16:34:57 -0300

    Basic tec maps plotting

--- plot/plotTECmaps.py
+++ plot/plotTECmaps.py
@@ -1,40 +1,102 @@
-# -*- coding: utf-8 -*-
+import os
+import pandas as pd
+import numpy as np
+from build import paths
+from plotConfig import *
+import datetime
+from utils import doy_str_format
+from terminator import *
+from __init__ import *
+from tecmap import MapMaker
+from scipy import interpolate
+from tecmap import *
+
+
+def plotROTImaps(i, save = True):
+    
+    
+    if save:
+        plt.ioff()
+        
+        
+    fig = plt.figure(figsize = (8, 8))
+    ax = plt.axes(projection = ccrs.PlateCarree())
+    
+    tecmap = roti_maps[i]
+    date = times[i]
+    
+    levels = np.linspace(0, 5, 50)
+    tecmap = np.where(tecmap < 0, np.nan, tecmap)
+    
+    
+    yy = np.linspace(-60.0, 40.0, tecmap.shape[1])
+    xx = np.linspace(-120.0, -20.0, tecmap.shape[0])
+
+    yy = np.arange(-30.0, 10.0, 0.5)
+    xx = np.arange(-70.0, -10.0, 0.5)
+    
+    img = ax.contourf(xx, yy, tecmap, levels = levels, cmap = "jet")
+ 
+
+    cbar_ax = fig.add_axes([0.95, 0.12, 0.03, 0.75]) #xposition, yposition, espessura, altura
+
+    cb = plt.colorbar(img, cax=cbar_ax)
+                      #ticks= [0, 1, 2, 3, 4, 5])
+
+    cb.set_label(r'ROTI (TECU/min)')
+
+    setting_states_map(ax)
+
+    ax.set(title = date)
+
+    a_lon_term, a_lat_term = terminator(date, 18)
+    
+    x = np.array(a_lon_term) 
+    y = np.array(a_lat_term)
+    f = interpolate.interp1d(x, y, fill_value="extrapolate")
+    
+    xnew = np.arange(-180, 180, 1)
+    ynew = f(xnew)
+    ax.plot(xnew, ynew,  "--", color = "k", lw = 3)
+ 
+    da = pd.read_csv("mag_inclination_2021.txt", 
+                     delim_whitespace = True)
+
+    da = pd.pivot_table(da, values = "B", 
+                        index = "lat", columns = "lon")
+
+    ax.contour(da.columns, da.index, da.values, 1, 
+               linewidths = 2, color = 'k',
+                transform = ccrs.PlateCarree())
+    
+    if save:
+        filename = str(date).replace("-", "").replace(" ", "").replace(":", "")
+        plt.savefig(f'img/maps/{filename}.png', 
+                    dpi = 80, bbox_inches="tight")
+    else:
+        plt.show()
+
+
+
 """
-Created on Sun Sep  4 20:23:57 2022
+for i, time in enumerate(times):
+    
+    print(i, time)
+    plotROTImaps(i, save = True)
+    
 
-@author: Luiz
+
+
+print(roti_maps[110].max())
+
 """
+#pd.DataFrame(roti_maps[1]).to_excel("test_roti.x")
 
-from plot.mapping import *
+plotROTImaps(110, save = False)
 
-start_lat = -40
-end_lat = 10
-end_lon = -30
-start_lon = -80
-fig, ax = plotMapping(start_lat = start_lat, 
-                end_lat = end_lat, 
-                start_lon = start_lon, 
-                end_lon = end_lon,
-                step_lat = 5, 
-                step_lon = 5)
 
-num = 30
-elevation = 0
+tecmap = roti_maps[110]
+xsize, ysize = tecmap.shape
+print(xsize, ysize)
+tecmap = np.where(tecmap < 0, np.nan, tecmap)
 
-sel_time = times[num]
-
-df = adf.loc[(adf.index > sel_time) & 
-             (adf.index < sel_time + delta) & 
-             (adf.el > elevation), :]
-
-ts = df.copy()
-
-lons, lats, arr = getting_values(ts, 
-               start_lat, end_lat, 
-               start_lon, end_lon, 
-               step = 1)
-
-ax.contourf(lons, lats, arr, 50, cmap = "jet")
-
-ax.set(title = str(sel_time))
-plt.show()
