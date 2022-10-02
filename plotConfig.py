4d4aa6c31dc42825bb5a944ced9411dabe1bd0ef
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2022-10-01 22:32:04 -0300

    join functions

--- plotConfig.py
+++ plotConfig.py
@@ -10,6 +10,10 @@ import terminator as tr
 from sub_ionospheric_point import convert_coords
 import json
 
+import shapely.geometry as sgeom
+from cartopy.geodesic import Geodesic
+#from plotMagneticEquator import plot_equator
+
 plt.rcParams.update({'font.size': 14, 
                      'axes.linewidth' : 0.5,
                      'grid.linewidth' : 0.5,
@@ -120,6 +124,59 @@ class plotting(object):
     
     
 
+
+
+
+def circle_range(ax, longitude, latitude, 
+                 radius = 500, color = "gray"):
+             
+    gd = Geodesic()
+
+    cp = gd.circle(lon = longitude, 
+                   lat = latitude, 
+                   radius = radius * 1000.)
+    
+    geoms = [sgeom.Polygon(cp)]
+
+    ax.add_geometries(geoms, crs=ccrs.PlateCarree(), 
+                      edgecolor = 'black', color = color,
+                      alpha = 0.2, label = 'radius')
+
+def circle_with_legend(ax, angle, height, name, color, infos, 
+                       marker = "s"):
+     
+     radius = height * np.sin(np.radians(angle)) 
+          
+     if radius < 1:
+         radius = 500
+     
+     lon, lat = tuple(infos[name])
+
+     circle_range(ax, lon, lat, radius = radius, 
+                  color = color)
+         
+     ax.plot(lon, lat, marker = marker, label =  name, 
+             color = color, markersize = 10,
+             transform = ccrs.PlateCarree())
+     
+     ax.legend(loc = "lower right")
+ 
+def save(infile = "img/methods/InstrumentionLocations.png"):     
+  
+    plt.savefig(infile, dpi = 500, bbox_inches = "tight")
+    
+def main():   
+    p = plotting()
+    
+    fig, ax = p.subplots_with_map(width = 15, heigth = 15, ncols = 1)
+    
+    p.setting_states_map(ax, step_lat = 1, step_lon = 1,
+                         lat_min = -12, lat_max = -2, 
+                         lon_max = -32, lon_min = -42)
+    
+    infos = {"Cariri": [-36.55, -7.38], 
+             "Fortaleza": [-38.45, -3.9]}
+    
 
 
 
