d12874b1a2a7b1e0c56dea8d6f8354558004b9fa
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2022-10-09 17:35:48 -0300

    Changing plot limits and adding new kword

--- plot/plotSequential.py
+++ plot/plotSequential.py
@@ -1,13 +1,17 @@
-from utils import doy_str_format
 from ROTI import *
 import matplotlib.dates as dates 
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import datetime 
 
 infile = "Database/all_process/2014/001_test/"
 
-def plot_tec_rate(df, prn, station, save = False):
+def plot_tec_rate(df, prn, station, 
+                  save = False, 
+                  path_to_save = None):
+    
+    """Plotting subplots for each TEC rate and slant TEC"""
     
     if save:
         plt.ioff()
@@ -20,7 +24,7 @@ def plot_tec_rate(df, prn, station, save = False):
     
     title = f"{prn.upper()} ({station.upper()}) - {date}"
     
-    fig, ax = plt.subplots(figsize = (10, 6), 
+    fig, ax = plt.subplots(figsize = (8, 10), 
                            nrows = 4, 
                            sharex = True)
     
@@ -30,52 +34,68 @@ def plot_tec_rate(df, prn, station, save = False):
 
     args = dict(color = "k", lw = 1)
     ax[0].plot(df1.el, **args)
-    ax[0].set(ylabel = "Elevation (°)", 
-              title = title)
+    ax[0].set(ylabel = "Elevação (°)", 
+              title = title, ylim = [0, 90])
 
     ax[1].plot(time, stec, **args)
-    ax[1].set(ylabel = "STEC (TECU)")
-    
+    ax[1].set(ylabel = "STEC (TECU)", ylim = [-40, 10])
+
     #indexes = find_gaps(df1).gaps
     dtec = df1["stec"] - df1["stec"].rolling("2.5min").mean()
     rot, rot_time, roti, roti_time = rot_and_roti(dtec, time)
-    #rtime, rot = func_rot(stec, time)
-    #drot = rot - running(rot)
+   
 
     ax[2].plot(rot_time, rot, **args)
-    ax[2].set(ylabel = "ROT")
+    ax[2].set(ylabel = "ROT (TECU/min)", ylim = [-12, 12])
+    
 
-    #roti_time, roti_values = func_roti(rtime, drot, step = 4, length = 1)
     ax[3].plot(roti_time, roti, **args)
 
 
-    delta_lim = timedelta(hours = 2)
-    ax[3].set(ylabel = "ROTI \n (TECU/min)", 
-              ylim = [0, 5], 
+    delta_lim = datetime.timedelta(hours = 1)
+    ax[3].set(ylabel = "ROTI (TECU/min)", 
+              ylim = [0, 6], 
+              yticks = np.arange(0, 6, 1),
               xlim = [df1.index[0] - delta_lim, 
                       df1.index[-1] + delta_lim], 
-              xlabel = "time (UT)")
+              xlabel = "Hora universal (UT)")
 
     ax[3].xaxis.set_major_formatter(dates.DateFormatter('%H'))
     ax[3].xaxis.set_major_locator(dates.HourLocator(interval = 1))
     
+    
     plt.rcParams.update({'font.size': 12})  
     if save:
         print("Saving...", prn)
-        fig.savefig(f"img/roti/{station}/{prn}.png", 
-                   dpi = 100, facecolor="white", transparent=False)
+        if path_to_save == None:
+            path_to_save = "img/roti/{station}/"
+        
+            
+        fig.savefig(f"{path_to_save}{prn}.png", 
+                   dpi = 100, 
+                   facecolor="white", 
+                   transparent=False)
         
         plt.clf()   
         plt.close()
     else:
         plt.show()
         
-        
-station = filename.replace(".txt", "")
-
-df = pd.read_csv(infile + filename, 
-         delim_whitespace = True, 
-         index_col = "time")
-
-df.index = pd.to_datetime(df.index)
-plot_tec_rate(df, prn, station, save = False)
+def main():
+    filename = "pbjp.txt"
+    prn = "G14"
+    station = filename.replace(".txt", "")
+    
+    df = pd.read_csv(infile + filename, 
+             delim_whitespace = True, 
+             index_col = "time")
+    
+    df.index = pd.to_datetime(df.index)
+    
+    
+    path_to_save = "G:\\My Drive\\Doutorado\\Modelos_Latex_INPE\\docs\\Proposal\\Figures\\methods\\"
+    
+    
+    plot_tec_rate(df, prn, 
+                  station, save = False, 
+                  path_to_save=path_to_save)
