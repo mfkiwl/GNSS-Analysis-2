07cd3720a7949a533b3f7ba2c4d1a24284c12679
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2022-11-12 19:43:37 -0300

    labeling

--- plot/plotNortheastReceivers.py
+++ plot/plotNortheastReceivers.py
@@ -1,63 +1,72 @@
 import pandas as pd
-from plotConfig import mapping
+import os
+from plotConfig import *
 from plot.plotMappingRange import *
 from build import paths, tex_path
 
-p = mapping(width = 20, heigth = 20, ncols = 1 )
+def plotNortheastInstrumentation():
+    p = mapping(width = 20, heigth = 20, ncols = 1 )
+    
+    
+    fig, ax = p.subplots_with_map()
+    
+    p.mapping_attrs(ax, 
+                    step_lat = 2, step_lon = 2,
+                    lat_min = -12, lat_max = -2, 
+                    lon_max = -32, lon_min = -42)
+    
+    
+    
+    plotStations(ax, 
+                date = datetime.date(2014, 1, 1), 
+                color = "green", 
+                markersize = 15, 
+                marker = "o",   
+                lat_min = -12, 
+                lat_max = -2, 
+                lon_max = -32, 
+                lon_min = -42)
+    
+    
+    plot_range_stations(ax)
+    
+    
+    size = 300
+    
+    l1 = plt.scatter([],[], s = size, 
+                     color = 'green', 
+                     marker = "o",
+                     edgecolors='none')
+    
+    
+    l2 = plt.scatter([],[], s = size, 
+                     color = 'red', 
+                     marker = 's', 
+                     edgecolors='none')
+    
+    
+    l3 = plt.scatter([],[], s = size, 
+                     color = 'blue', 
+                     marker = '^', 
+                     edgecolors='none')
+    
+    labels = ["Receptores GNSS - RBMC", 
+              "Imageador (Cariri)", 
+              "Ionossonda (Fortaleza)"]
+    
+    leg = plt.legend([l1, l2, l3], labels, 
+                     fontsize = 30,
+                     loc = "upper right", 
+                     )
+    
+    return fig
 
 
-fig, ax = p.subplots_with_map()
+fig = plotNortheastInstrumentation()
 
-p.mapping_attrs(ax, 
-                step_lat = 2, step_lon = 2,
-                lat_min = -12, lat_max = -2, 
-                lon_max = -32, lon_min = -42)
+path_to_save = os.path.join(tex_path("results"), 
+                            "northeast_region.png")
+#print(path_to_save)
 
-
-
-plotStations(ax, 
-            date = datetime.date(2014, 1, 1), 
-            color = "green", 
-            markersize = 15, 
-            marker = "o",   
-            lat_min = -12, 
-            lat_max = -2, 
-            lon_max = -32, 
-            lon_min = -42)
-
-
-plot_range_stations(ax)
-
-
-size = 300
-
-l1 = plt.scatter([],[], s = size, 
-                 color = 'green', 
-                 marker = "o",
-                 edgecolors='none')
-
-
-l2 = plt.scatter([],[], s = size, 
-                 color = 'red', 
-                 marker = 's', 
-                 edgecolors='none')
-
-
-l3 = plt.scatter([],[], s = size, 
-                 color = 'blue', 
-                 marker = '^', 
-                 edgecolors='none')
-
-labels = ["Receptores GNSS - RBMC", 
-          "Imageador (Cariri)", 
-          "Ionossonda (Fortaleza)"]
-
-leg = plt.legend([l1, l2, l3], labels, 
-                 fontsize = 30,
-                 loc = "upper right", 
-                 )
-
-filename = "northeast_region.png"
-fig.savefig(os.path.join(tex_path("results"), filename),
-            dpi = 500
-            )
+fig.savefig(path_to_save ,
+            dpi = 300)
