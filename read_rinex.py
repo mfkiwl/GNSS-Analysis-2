ae6792d0d28c37d506a560037f434b8a035615e7
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2022-12-10 00:22:21 -0300

    updating

--- read_rinex.py
+++ read_rinex.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 from datetime import datetime, timedelta
 
 infile = "database/rinex/2014/alar0011.14o"
@@ -89,10 +89,48 @@ def get_prns(epoch):
                           range(3, len(prns[2:]) + 3, 3)])        
     return prns_list
 
-epoch, indexes = get_epoch(lines)
-prns_list = get_prns(epoch)
 
+
+
+    rows = []
+    for i in elem_list:
+        observable = float(i[:-2])
+        lockphase = i[-2:-1]
+        if lockphase == " ":
+            lockphase = np.nan
+        else:
+            lockphase = int(i[-2:-1])
+        rows.append(observable)
+        rows.append(lockphase)
+        
+    return rows
 num = 0
-elem = lines[indexes[num] + 2: indexes[num + 1]]
 
-print(elem[0].lstrip())
+
+def get_rows(elem_list):
+    epoch, indexes = get_epoch(lines)
+    elem = lines[indexes[num] + 2: indexes[num + 1]]
+    
+    prns_list = get_prns(epoch)
+
+    
+    sv = prns_list[num]
+    columns = []
+    for j in range(len(elem)):
+            
+        elemlist = elem[j].lstrip().replace("\n", "")
+        
+        L1 = elemlist[0:13]
+        L1lli = elemlist[13:14]
+        L1ssi = elemlist[14:15]
+        C1 = elemlist[17:29]
+        C1ssi = elemlist[30:31]
+        L2 = elemlist[32:45].strip()
+        L2lli = elemlist[45:46]
+        P2 = elemlist[47:61]
+        L2ssi = elemlist[61:62]
+        P2ssi = elemlist[62:63]
+        
+        columns.append([sv[j], L1, L1lli, C1, L2, L2lli, P2])    
+        
+    print(columns)
