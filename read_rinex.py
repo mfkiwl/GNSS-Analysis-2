fcd324fcaaa5ddf2666e0bdddcc21b44e5dac38a
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2022-12-09 22:12:24 -0300

    get first time

--- read_rinex.py
+++ read_rinex.py
@@ -1,4 +1,6 @@
 
+from datetime import datetime
+
 infile = "database/rinex/2014/alar0011.14o"
 
 def get_header(infile):
@@ -16,7 +18,9 @@ def get_header(infile):
             
     return header 
 
-def attributes(header):
+def attributes(infile):
+    
+    header = get_header(infile)
     
     dict_infos = {}
     
@@ -32,16 +36,48 @@ def attributes(header):
     
     return dict_infos
 
+
+def start_datetime(infile):
+
+    attrs = attributes(infile)
+    
+    first_obs = attrs['TIMEOFFIRSTOBS']
+    
+    
+    year = int(first_obs[0])
+    month = int(first_obs[1])
+    day = int(first_obs[2])
+    
+    hour = int(first_obs[3])
+    minute = int(first_obs[4])
+    sec = int(float(first_obs[5]))
+
+    return datetime(year, month, day, 
+                    hour, minute, sec)
+
+
 lines = open(infile, "r").readlines()
 
-#start_data = lines.find("END OF HEADER")
 
-
-#outline = lines[start_data:].strip()
-
-        
-current = lines[15:33]
-
-
-print(current[0])
-
+def get_data(lines):
+    current = lines[15:33]
+    
+    
+    epoch = (current[0].replace("\n", "") + current[1].strip()).split("  ")
+    
+    
+    prns = epoch[-1].split(" ")[1]
+    
+    prns_list = [prns[2:][y - 3: y] for y in 
+                 range(3, len(prns[2:]) + 3, 3)]
+    
+    
+    
+    one = current[2:][0].split("  ")
+    
+    #for i in range(len(one)):
+       # print(one[i][:14])
+       
+    print(one)
+    #date = start_datetime(infile)
+get_data(lines)
